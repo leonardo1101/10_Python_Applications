8eed014b5a5097c95ec7c399603a40cab6af900b
leonardo1101 <leonardo98ti@gmail.com>
2020-07-06 21:35:36 -0300

    First application: English dictionary.

---
+++ 1_Application_English_Dictionary/app.py
@@ -0,0 +1,34 @@
+import json
+from difflib import get_close_matches
+
+with open("data.json") as dictionary:
+    dictionary_data = json.load(dictionary)
+
+def traslate(word):
+    word = word.lower()
+    if word in dictionary_data:
+        return dictionary_data[word]
+    elif word.upper() in dictionary_data:
+        return dictionary_data[word.upper()]
+    elif word.capitalize() in dictionary_data:
+        return dictionary_data[word.capitalize()]
+    elif get_close_matches(word, dictionary_data.keys(), n=1, cutoff=0.8):
+        prob_word = get_close_matches(word, dictionary_data.keys(),
+                n=1, cutoff=0.8)[0]
+        res = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
+                prob_word)
+        if res == "Y":
+            return dictionary_data[prob_word]
+        elif res != "N":
+            return "We didn't undestand your entry."
+    return "The word doesn't exist. Please double check it."
+
+word = input("Enter word: ")
+result = traslate(word)
+
+if isinstance(result, list):
+    number_definitions = len(result)
+    for def_index in range(number_definitions):
+        print("%dº Definition: %s" % (def_index + 1, result[def_index]))
+else:
+    print(result)
