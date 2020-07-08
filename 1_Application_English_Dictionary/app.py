2e287610a8406e5dc6daffe4fac82ec101ebab6b
leonardo1101 <leonardo98ti@gmail.com>
2020-07-07 22:01:53 -0300

    First application: Use a database to search the word definition

--- 1_Application_English_Dictionary/app.py
+++ 1_Application_English_Dictionary/app.py
@@ -1,33 +1,32 @@
-import json
+import mysql.connector
+from dictionary_data_base import *
 from difflib import get_close_matches
 
-with open("data.json") as dictionary:
-    dictionary_data = json.load(dictionary)
-
 def traslate(word):
+    # Load words from dictionary
+    words = get_all_words_in_dictionary()
     word = word.lower()
     # Check if the word is in the dictionary
-    if word in dictionary_data:
-        return dictionary_data[word]
-    elif word.upper() in dictionary_data:
-        return dictionary_data[word.upper()]
-    elif word.capitalize() in dictionary_data:
-        return dictionary_data[word.capitalize()]
+    if word in words:
+        return get_possible_definitions(word)
+    elif word.upper() in words:
+        return  get_possible_definitions(word.upper())
+    elif word.capitalize() in words:
+        return get_possible_definitions(word.capitalize())
     # If the word isn't in the dictionary we will try find the most similar
-    elif get_close_matches(word, dictionary_data.keys(), n=1, cutoff=0.8):
-        prob_word = get_close_matches(word, dictionary_data.keys(),
+    elif get_close_matches(word, words, n=1, cutoff=0.8):
+        prob_word = get_close_matches(word, words,
                 n=1, cutoff=0.8)[0]
         res = input("Did you mean %s instead? Enter Y if yes, or N if no: " %
                 prob_word)
         if res == "Y":
-            return dictionary_data[prob_word]
+            return get_possible_definitions(prob_word)
         elif res != "N":
             return "We didn't undestand your entry."
     return "The word doesn't exist. Please double check it."
 
 word = input("Enter word: ")
 result = traslate(word)
-
 # Show each definition
 if isinstance(result, list):
     number_definitions = len(result)
---
+++ 1_Application_English_Dictionary/dictionary_data_base.py
@@ -0,0 +1,20 @@
+import mysql.connector
+from myconfig import *
+
+con = mysql.connector.connect(user = USER, password = PASSWORD, host = HOST,
+        database = DATABASE)
+
+cursor = con.cursor()
+
+
+def get_all_words_in_dictionary():
+    query = cursor.execute("SELECT Expression FROM Dictionary")
+    word_list = cursor.fetchall()
+    return [word[0] for word in word_list]
+
+
+def get_possible_definitions(word):
+    query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'"
+            % word)
+    definition_list = cursor.fetchall()
+    return [definition[0] for definition in definition_list]
