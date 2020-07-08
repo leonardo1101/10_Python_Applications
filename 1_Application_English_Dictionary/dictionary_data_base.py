f4c07f222ff0794a9496a5a46834ee2a0755c9b4
leonardo1101 <leonardo98ti@gmail.com>
2020-07-07 22:03:39 -0300

    First application: Remove some blank lines

--- 1_Application_English_Dictionary/dictionary_data_base.py
+++ 1_Application_English_Dictionary/dictionary_data_base.py
@@ -3,15 +3,12 @@ from myconfig import *
 
 con = mysql.connector.connect(user = USER, password = PASSWORD, host = HOST,
         database = DATABASE)
-
 cursor = con.cursor()
-
 
 def get_all_words_in_dictionary():
     query = cursor.execute("SELECT Expression FROM Dictionary")
     word_list = cursor.fetchall()
     return [word[0] for word in word_list]
-
 
 def get_possible_definitions(word):
     query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'"
